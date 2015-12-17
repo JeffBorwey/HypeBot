4ebcaec501fa22f1d811ea8defa2c01de9d8d4a2
JeffBorwey <jeffborwey@gmail.com>
2015-12-16 18:21:53 -0600

    Add printing of when channels are joined and the bot is finally initialized

--- functionClasses/Lenny/LennyFaceHandler.py
+++ functionClasses/Lenny/LennyFaceHandler.py
@@ -3,8 +3,10 @@
 import urllib2
 from functionClasses import AbstractHandler
 from lxml import html
+import random
 
 LENNY_SEARCH_URL = 'http://textsmili.es/?cr=1'
+LENNY_SEARCH_2 = 'https://textfac.es'
 
 
 class LennyFaceHandler(AbstractHandler.AbstractHandler):
@@ -17,11 +19,10 @@ class LennyFaceHandler(AbstractHandler.AbstractHandler):
 
     def handle(self, message, from_name_full, msg_obj):
         # message should be sanitised
-        html_str = self.opener.open(LENNY_SEARCH_URL).read()
+        html_str = self.opener.open(LENNY_SEARCH_2).read()
 
         tree = html.fromstring(html_str)
-        button = tree.xpath("//button[@class = 'btn btn-default unicomp-do-copy']")
-        print(vars(button))
-        print(vars(button.attrib))
-        face = button.attrib['data-clipboard-text']
+        buttons = tree.xpath("//button[@class = 'facebtn center-block btn-material btn-material-default ']")
+        rand_index = random.randint(0, len(buttons)-1)
+        face = buttons[rand_index].attrib['data-clipboard-text']
         return face
